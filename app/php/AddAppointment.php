da7069dd9f3aac065e207581a5262a58a31538ca
ramieb11 <barakatr@email.uscupstate.edu>
2015-10-13 10:39:19 -0400

    User interface for appointments ready
    I got the add appointments popup to show. I'm having trouble with the
    calendar. Also I added the dropdown menu for tutor name, but was
    thinking if you can link it to show the tutors that have submitted a
    schedule that would be an option!

--- app/php/AddAppointment.php
+++ app/php/AddAppointment.php
@@ -5,23 +5,27 @@
         <h4 id="add-appointment-modal-label">Make an Appointment</h4>
       </div>
       <div class="dropdown">
-		<button class="btn btn-default dropdown-toggle" type="button" id="select-tutor-dropdown" data-toggle="Tutor Name" aria-haspopup="true" aria-expanded="true">
-		<label>Select Tutor Name</label>
-		<span class="caret"></span>
-		</button>
+		<label class="col-xs-3 control-label">Select Tutor:</label>
+			<button class="btn btn-default dropdown-toggle" type="button" id="select-tutor-dropdown" data-toggle="Tutor Name" aria-haspopup="true" aria-expanded="true">
+				<span class="caret"></span>
+			</button>
 		<ul class="dropdown-menu" aria-labelledby="select-tutor-dropdown">
 			<li><a href="#">Johnathan Sullivan</a></li>
 			<li><a href="#">Someone Else</a></li>
 		</ul>
 	  </div>
-	  <label>Select Date: </label>
-		<div id="datepicker" class="input-group date" data-date-format="mm-dd-yyyy">
-			<input class="form-control" type="text" readonly />
-			<span class="input-group-addon"><i class="glyphicon glyphicon-calendar"></i></span>
+	<div class="control-group">
+        <label class="col-xs-3 control-label" for="date-picker-2" class="control-label">Select Date:</label>
+            <div class="input-group">
+                <input id="date-picker-2" type="text" class="date-picker form-control" />
+                <label for="date-picker-2" class="input-group-addon btn"><span class="glyphicon glyphicon-calendar"></span>
+                </label>
+            </div>
+    </div>
+		<div class="timepicker">
+			<label class="col-xs-3 control-label">Select Time:</label>
+			<input type="time" name="usr_time">
 		</div>
-	  <form action="action_page.php">
-		<label>Select Time: </label>
-		<input type="time" name="usr_time">
 	  </form>
       <div class="modal-footer">
         <div>
@@ -45,13 +49,19 @@
     <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
     <label id="success-alert-label"></label>
  </div>
-</script>
-
 <script type="text/javascript">
 $(function() {
   $('#add-appointment-modal').on('hidden.bs.modal', function () {
     clearInputs();
   });
+});
+
+$(".date-picker").datepicker();
+
+$(".date-picker").on("change", function () {
+    var id = $(this).attr("id");
+    var val = $("label[for='" + id + "']").text();
+    $("#msg").text(val + " changed");
 });
 
 function clearInputs() {
