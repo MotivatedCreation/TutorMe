1c0b5fb2cb82f935516b97ffb2a359c121eadfca
ramieb11 <barakatr@email.uscupstate.edu>
2015-11-05 14:58:03 -0500

    Fixing Issue #26
    Removing extra open button under Tutor level

--- app/php/Assignments.php
+++ app/php/Assignments.php
@@ -72,8 +72,6 @@
 						<label id="assignments-title-label" style="font-weight: normal;"><%= title %></label>
 						<p><textarea id="assignments-description-label" class="well well-sm" readonly="true"><%= description %></textarea></p>
 						<p>
-							<a href="<%=url%>" target="blank" class="btn btn-default" role="button">Open</a>
-						<p>
 							<a href="<%= url %>" target="blank" class="btn btn-default" role="button">Open</a>
 						</p>
 					</div>
